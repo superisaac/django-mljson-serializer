3b91699e50a4b48857e9288e30535211521a6ca5
Zeng Ke <superisaac.ke@gmail.com>
2015-12-16 14:44:14 +0800

    ver 0.1.1, added setup.cfg

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'django_mljson',
     packages = ['django_mljson'],
-    version = '0.1',
+    version = '0.1.1',
     description = 'django multi-line json serializer',
     author = 'Zeng Ke',
     author_email = 'superisaac.ke@gmail.com',
     url = 'https://github.com/superisaac/django-mljson-serializer',
-    download_url = 'https://github.com/superisaac/django-mljson-serializer/tarball/0.1',
+    download_url = 'https://github.com/superisaac/django-mljson-serializer/tarball/0.1.1',
     keywords = ['django', 'json', 'serializer'],
     classifiers = [],
 )
